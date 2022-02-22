0f80037f80fb6b3a7a2e0be9c7800d5fd169e810
Akankshasharma1516 <akshuagca26@gmail.com>
2022-02-22 11:49:04 +0530

    inital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form';
+  firstname: any;
+  lastname: any;
+  dob:any;
+  gender:any;
+  Email:any;
+  Phonenumber:any;
+  form = new FormGroup({
+    firstname: new FormControl("", [Validators.required]),
+    lastname: new FormControl("",[Validators.required]),
+    dob:new FormControl(null,Validators.required),
+    gender: new FormControl("",[Validators.required]),
+    Email:new FormControl("",[Validators.required,Validators.email]),
+    Phonenumber: new FormControl("",[Validators.required,Validators.minLength(10)])
+  });
+
+  onSubmit() {
+    console.log(this.form.value);
+    this.firstname = this.form.get('firstname')?.value;
+    this.lastname = this.form.get('lastname')?.value;
+    this.dob = this.form.get('dob')?.value;
+    this.gender = this.form.get('gender')?.value;
+    this.Email = this.form.get('Email')?.value;
+    this.Phonenumber = this.form.get('Phonenumber')?.value;
+
+
+  }
 }
+
+
